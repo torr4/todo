e185645d7c52cd1ac084011f5a912e3c795b65fc
torr4 <tjimenez.feina@gmail.com>
2023-04-13 20:42:33 +0200

    init_repo

---
+++ src/Request.php
@@ -0,0 +1,16 @@
+<?php
+
+    namespace App;
+
+    use Psr\Http\Message\RequestInterface;
+
+    class Request {
+        protected $action;
+        protected $controller;
+        protected $method;
+
+        function __construct(){
+            
+        }
+
+    } 
